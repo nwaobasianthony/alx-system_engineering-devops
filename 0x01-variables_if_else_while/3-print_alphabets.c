02a743504e24c21d0abfb080d211298d88c6555d
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-03-19 02:04:52 +0100

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+    char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+    while (C <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
