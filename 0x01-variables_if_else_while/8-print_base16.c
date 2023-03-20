d61b19d982428ca1984c4499138b752f2caa4e3e
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-03-20 04:46:13 +0100

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all the numbers
+ * of base 16 in lowercase, followed by a new line
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	char x;
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
