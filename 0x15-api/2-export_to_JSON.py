ec3fbc8154f9fe33baf3d8dddc1698c288bba205
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-10-25 06:06:06 +0100

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-'''A script that gathers data from an API and exports it to a CSV file.
-'''
-import re
+"""
+A Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress and
+export data in the JSON format.
+"""
+
+import json
 import requests
-import sys
-
-
-API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
+from sys import argv
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('username')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            with open('{}.csv'.format(id), 'w') as file:
-                for todo in todos:
-                    file.write(
-                        '"{}","{}","{}","{}"\n'.format(
-                            id,
-                            user_name,
-                            todo.get('completed'),
-                            todo.get('title')
-                        )
-                    )
+    eid = argv[1]
+    userUrl = "https://jsonplaceholder.typicode.com/users"
+    url = userUrl + "/" + eid
+
+    response = requests.get(url)
+    uname = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    dic = {eid: []}
+    for task in tasks:
+        dic[eid].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": uname
+        })
+
+    with open('{}.json'.format(eid), 'w') as f:
+        json.dump(dic, f)
